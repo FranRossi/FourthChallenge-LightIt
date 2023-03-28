dd3ff2469fb53d24c727a2fcc43ba697097f6223
Francisco Rossi <francisco.rossi@lightit.io>
2023-03-28 11:24:32 -0300

    Adds first route to see all cities, and cretes a new index blade file to display all of the cities stored in the database

--- fourth-challenge/app/Http/Controllers/CityController.php
+++ fourth-challenge/app/Http/Controllers/CityController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\City;
 use Illuminate\Http\Request;
 
 class CityController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('cities.index',[
+            'cities' => City::latest()->paginate(5)
+        ]);
+    }
 }
