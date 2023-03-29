b7d11fa8424bf04949252f74b5f2d769ff52c6a4
Francisco Rossi <francisco.rossi@lightit.io>
2023-03-29 09:15:09 -0300

    Separate Ajax with Jquery script to a separete javascript file located in public directory

--- fourth-challenge/resources/views/components/table.blade.php
+++ fourth-challenge/resources/views/components/table.blade.php
@@ -86,21 +86,4 @@
     </div>
 </div>
 
-<script>
-    $(document).ready(function() {
-        $('.delete-button').click(function(e) {
-            e.preventDefault();
-
-            // Send an AJAX request to the server
-            $.ajax({
-                url: $('.delete-form').attr('action'),
-                type: 'DELETE',
-                data: $('.delete-form').serialize(),
-                success: function() {
-                    // Remove the deleted item from the list
-                    $('.delete-form').closest('tr').remove();
-                }
-            });
-        });
-    });
-</script>
+<script src="{{ asset('js/citiesCRUD.js') }}"></script>
