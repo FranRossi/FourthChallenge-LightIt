b7cbae00cf7b1de5203e1bc09679a9600808901a
Francisco Rossi <francisco.rossi@lightit.io>
2023-03-29 09:29:38 -0300

    Creates separete blade component for table headers

--- fourth-challenge/resources/views/components/table.blade.php
+++ fourth-challenge/resources/views/components/table.blade.php
@@ -5,50 +5,10 @@
         <table class="min-w-full divide-y divide-gray-300">
             <thead>
             <tr>
-                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900 ">
-                    <a href="#" class="group inline-flex">
-                        Id
-                        <!-- Active: "bg-gray-200 text-gray-900 group-hover:bg-gray-300", Not Active: "invisible text-gray-400 group-hover:visible group-focus:visible" -->
-                        <span class="invisible ml-2 flex-none rounded text-gray-400 group-hover:visible group-focus:visible">
-                                                <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                                  <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
-                                                </svg>
-                                              </span>
-                    </a>
-                </th>
-                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                    <a href="#" class="group inline-flex">
-                        Name
-                        <!-- Active: "bg-gray-200 text-gray-900 group-hover:bg-gray-300", Not Active: "invisible text-gray-400 group-hover:visible group-focus:visible" -->
-                        <span class="ml-2 flex-none rounded bg-gray-100 text-gray-900 group-hover:bg-gray-200">
-                                                <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                                  <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
-                                                </svg>
-                                              </span>
-                    </a>
-                </th>
-                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                    <a href="#" class="group inline-flex">
-                        Flights Departing
-                        <!-- Active: "bg-gray-200 text-gray-900 group-hover:bg-gray-300", Not Active: "invisible text-gray-400 group-hover:visible group-focus:visible" -->
-                        <span class="invisible ml-2 flex-none rounded text-gray-400 group-hover:visible group-focus:visible">
-                                                <svg class="invisible ml-2 h-5 w-5 flex-none rounded text-gray-400 group-hover:visible group-focus:visible" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                                  <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
-                                                </svg>
-                                              </span>
-                    </a>
-                </th>
-                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                    <a href="#" class="group inline-flex">
-                        Flights Arriving
-                        <!-- Active: "bg-gray-200 text-gray-900 group-hover:bg-gray-300", Not Active: "invisible text-gray-400 group-hover:visible group-focus:visible" -->
-                        <span class="invisible ml-2 flex-none rounded text-gray-400 group-hover:visible group-focus:visible">
-                                                <svg class="invisible ml-2 h-5 w-5 flex-none rounded text-gray-400 group-hover:visible group-focus:visible" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                                  <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
-                                                </svg>
-                                              </span>
-                    </a>
-                </th>
+               <x-table-header :name="'Id'"/>
+               <x-table-header :name="'Name'"/>
+               <x-table-header :name="'Flights Departing'"/>
+               <x-table-header :name="'Flights Arriving'"/>
                 <th scope="col" class="relative px-3 py-3.5">
                     <span class="sr-only">Edit</span>
                 </th>
