498ba25f985f8049c2c0e111369ab87681f85e4e
Francisco Rossi <francisco.rossi@lightit.io>
2023-04-04 09:24:56 -0300

    Adds a new Test file for cities

--- fourth-challenge/tests/Feature/Cities/CityTest.php
+++ fourth-challenge/tests/Feature/Cities/CityTest.php
@@ -2,23 +2,45 @@
 
 namespace Tests\Feature\Cities;
 
+use App\Http\Controllers\CityController;
 use App\Models\City;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class CityTest extends TestCase
 {
-    use RefreshDatabase;
+
+    /** @test */
+    public function store_new_city_with_form()
+    {
+        $this->withoutExceptionHandling();
+
+        $this->post(action([CityController::class, 'store']),
+            [
+            'name' => 'Miami',
+            'flights_arriving' => 100,
+            'flights_departing' => 100,
+        ]);
+
+        $this->assertDatabaseHas(City::class, [
+            'name' => 'Miami',
+            'flights_arriving' => 100,
+            'flights_departing' => 100,
+        ]);
+    }
+
     /** @test */
     public function index()
     {
+        $this->withoutExceptionHandling();
+
         City::factory()->count(2)->sequence(
             ['name' => 'Miami'],
             ['name' => 'London'],
         )
         ->create();
 
-        $this->get('/')
+        $this->get('/cities')
             ->assertSuccessful()
             ->assertSee('Miami')
             ->assertSeeInOrder([
@@ -26,5 +48,7 @@ class CityTest extends TestCase
                 'London',
             ]);
     }
+
+
 
 }
